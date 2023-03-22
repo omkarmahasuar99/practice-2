d747ebbfa9008e924766edbe81aa1d710144853a
omkarmahasuar99 <126793930+omkarmahasuar99@users.noreply.github.com>
2023-03-22 12:09:38 +0530

    increase score by 5

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        double score= 89.0;
+        if(score>=80 && score<=90)
+            score=score+5;
+        System.out.println(score);
+    }
+}
